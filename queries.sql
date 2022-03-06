3ce2150dd36047515332abcbd6c5584d17841a14
Natasha Matthews <natashamatthews90@gmail.com>
2022-03-06 12:24:55 -0600

    finished part 2 of assigment

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 -- Part 1: Test it with SQL
+select column_name, data_type
+from jobs;
 
 -- Part 2: Test it with SQL
-
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
 
 -- Part 4: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required")
+    @Size(max = 75, message = "Location must not be more that 75 characters.")
+    private String location;
 
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
